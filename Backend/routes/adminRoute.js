7d79051b15beea827a8d10b6ad3cad850a00d750
irfanpm <muhammmedirfanpm@gmail.com>
2023-11-10 18:28:03 +0530

    admin backend

--- Backend/routes/adminRoute.js
+++ Backend/routes/adminRoute.js
@@ -1,3 +1,15 @@
 const express=require('express')
 const admin=express.Router()
+const {getAllService,getAllUser,getCategoryService,getServiceById,getUserById,isBlockService,isBlockUser}=require('../controller/admin')
+const adminAuth=require('../middleware/adminJWTAuthenticaton')
 
+admin.gwt('/admin/getusers',getAllUser)
+admin.get('/admin/getservices',getAllService)
+admin.post('/admin/getservice',getServiceById)
+admin.post('/admin/getuser',getUserById)
+admin.post('/admin/userblock',isBlockUser)
+admin.post('/admin/serviceblock',isBlockService)
+
+
+
+module.exports=admin
---
+++ frontend/app/admin/page.jsx
@@ -0,0 +1,12 @@
+import Dashboard from '@/components/admin/Admindash'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Dashboard/>
+    </div>
+  )
+Dashboard}
+
+export default page
